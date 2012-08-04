6eba3dddc72afc771475b110cd17cf1812b545af
Andrew Ferrante <rpxxxiv@gmail.com>
2012-08-04 06:09:27 -0700

    Initialize

---
+++ jQueryAudioPlayerSandbox/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("jQueryAudioPlayerSandbox")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("jQueryAudioPlayerSandbox")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4de64d78-2b03-4191-9cb8-e7762b6dfc7f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ jQueryAudioPlayerSandbox/Scripts/AudioLinks.js
@@ -0,0 +1,14 @@
+﻿var $AudioLinks =
+[
+    { Title: 'Porch Table', Path: '01 Porch Table', Type: 'omw' },
+    { Title: 'Red Chicken', Path: '02 Red Chicken', Type: 'omw' },
+    { Title: 'Mene', Path: '03 Mene', Type: 'omw' },
+    { Title: 'Pele', Path: '04 Pele', Type: 'omw' },
+    { Title: 'Chamaco', Path: '05 Chamaco', Type: 'omw' }
+]
+
+
+    $(window).ready(
+    function () {
+        
+    });
